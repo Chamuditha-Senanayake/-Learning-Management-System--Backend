375611b1c87008ec1588bb1b884a6a7d46e60fd9
chamcbs <chamudithacbs2@gmail.com>
2022-10-03 22:10:22 +0530

    Project initiation

---
+++ src/main/java/com/lms/Learning_Management_SystemBackend/LearningManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lms.Learning_Management_SystemBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lms/Learning_Management_SystemBackend/LearningManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.Learning_Management_SystemBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
